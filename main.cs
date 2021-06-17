1bbe7640597958f5ffd168fe2ea9f6c0bb8e622f
Creator125OCA <85368990+Creator125@users.noreply.github.com>
2021-06-17 20:15:17 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Main {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
+
+public class Espada{//Abriendo la clase espada
+//Atributos
+int largo;
+int peso;
+bool Estado;
+
+public static bool cortar(){//Abriendo el metodo cortar
+  //Acciones
+  bool filosa = true;
+  return filosa;
+}//Cerrando el metodo cortar
+
+public static void Destruir(){
+  //Acciones
+  bool partir = true;
+  return partir;
+}
+
+}//Cerrando la clase espada
